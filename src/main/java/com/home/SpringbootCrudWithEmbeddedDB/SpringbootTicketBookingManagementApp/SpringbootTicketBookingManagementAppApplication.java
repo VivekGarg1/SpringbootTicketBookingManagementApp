21a68f35750ebc311dc89cfb228a2673c23b9163
VivekGargHays <vivek.garg@hays.com>
2020-03-16 14:16:55 +0530

    code merge

--- src/main/java/com/home/SpringbootCrudWithEmbeddedDB/SpringbootTicketBookingManagementApp/SpringbootTicketBookingManagementAppApplication.java
+++ src/main/java/com/home/SpringbootCrudWithEmbeddedDB/SpringbootTicketBookingManagementApp/SpringbootTicketBookingManagementAppApplication.java
@@ -3,6 +3,8 @@ package com.home.SpringbootCrudWithEmbeddedDB.SpringbootTicketBookingManagementA
 
 import com.home.SpringbootCrudWithEmbeddedDB.SpringbootTicketBookingManagementApp.model.Ticket;
 import com.home.SpringbootCrudWithEmbeddedDB.SpringbootTicketBookingManagementApp.service.TicketBookingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -17,17 +21,29 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-
+@RestController
 @SpringBootApplication
 @EnableAsync
 public class SpringbootTicketBookingManagementAppApplication implements CommandLineRunner {
+
+	private static final Logger logger= LoggerFactory.getLogger(SpringbootTicketBookingManagementAppApplication.class);
 
 	@Autowired
 	private TicketBookingService ticketBookingService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootTicketBookingManagementAppApplication.class, args);
+		logger.error("Message lohgged at error level");
+		logger.warn("Message lohgged at warn level");
+		logger.info("Message lohgged at info level");
+		logger.debug("Message lohgged at debug level");
 	}
+
+	@RequestMapping("/")
+	public String welcome(){
+		logger.info("Message lohgged at info level");
+		return "Hello World!!!";
+		}
 
 	@Override
 	public void run(String... args) throws Exception {
