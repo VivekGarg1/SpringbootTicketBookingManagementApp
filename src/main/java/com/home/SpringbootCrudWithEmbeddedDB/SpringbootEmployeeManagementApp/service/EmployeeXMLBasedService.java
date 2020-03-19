831583bb7493c80db32f7e631000f5cd9361cdd9
VivekGargHays <vivek.garg@hays.com>
2020-03-19 14:54:00 +0530

    code merge

--- src/main/java/com/home/SpringbootCrudWithEmbeddedDB/SpringbootEmployeeManagementApp/service/EmployeeXMLBasedService.java
+++ src/main/java/com/home/SpringbootCrudWithEmbeddedDB/SpringbootEmployeeManagementApp/service/EmployeeXMLBasedService.java
@@ -5,7 +5,7 @@ public class EmployeeXMLBasedService {
     private String name;
 
     public String getName() {
-        return name;
+        return  name;
     }
 
     public void setName(String name) {
